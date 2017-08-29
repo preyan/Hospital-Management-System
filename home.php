992c67bcec4cddeea40609abe02b07ed0b5da765
Preyan Bhowmick <preyan1997@gmail.com>
2017-08-29 22:34:51 +0530

    Initial Design

---
+++ home.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>M.K.Ghosh Hospital</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="container">
+	<header>
+		<p>M. K. GHOSH HOSPITAL<p>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="#">Packages</a></li>
+			<li><a href="#">Entry</a></li>
+		</ul>
+	</nav>
+</div>
+</body>
