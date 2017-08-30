9688342d958edf3481ac33f28ab0fbb112625fb5
Preyan Bhowmick <preyan1997@gmail.com>
2017-08-30 16:56:54 +0530

    changes

---
+++ admin.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>M.K.Ghosh Hospital</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="container">
+	<header>
+		M. K. GHOSH HOSPITAL
+	</header>
+	<nav>
+		<ul>
+		  <li><a href="#home">HOME</a></li>
+		  <li><a href="#packages">PACKAGES</a></li>
+		  <li id="admin"><a href="#admin">ADMIN</a></li>
+		</ul>
+	</nav>
+	
+</div>
+</body>
+</html>
--- home.php
+++ home.php
@@ -1,3 +1,7 @@
+<?php
+include('conn.php');
+?>
+
 <html>
 <head>
 <title>M.K.Ghosh Hospital</title>
@@ -6,20 +10,46 @@
 <body>
 <div class="container">
 	<header>
-		<p>M. K. GHOSH HOSPITAL<p>
+		M. K. GHOSH HOSPITAL
 	</header>
 	<nav>
 		<ul>
-		  <li><a href="#home">Home</a></li>
-		  <li><a href="#packages">Packages</a></li>
-		  <li class="dropdown">
-		    <a href="javascript:void(0)" class="dropbtn">Login</a>
-		    <div class="dropdown-content">
-		      <a href="#">Staff</a>
-		      <a href="#">Admin</a>
-		    </div>
-		  </li>
+		  <li><a href="#home">HOME</a></li>
+		  <li><a href="#packages">PACKAGES</a></li>
+		  <li id="admin"><a href="#admin">ADMIN</a></li>
 		</ul>
 	</nav>
+	<form  action="" method="post">
+	<table id="login">
+		<tr>
+			<td>USER-ID :</td><td><input type="text" name="f_userid"></td>
+		</tr>
+		<tr>
+			<td>PASSWORD :</td><td><input type="password" name="f_pass"></td>
+		</tr>
+		<tr>
+			<td align="center"><input type="reset" value="Reset"></td>
+			<td align="center"><input type="submit" name="loginbtn" value="Login"></td>
+		</tr>
+		<th><?php echo $error; ?></th>
+	</table>
+	</form>
 </div>
 </body>
+</html>
+
+<?php
+if(isset($_POST['loginbtn']))
+{
+	$result=mysql_query("SELECT * FROM staff WHERE id = '".$_POST['f_userid']."' AND pass = '".$_POST['f_pass']."'");
+	$n=mysql_num_rows($result);
+	if($n==1)
+	{
+		header('location:home1.php');
+	}
+	else
+	{
+		$error = "Your User-Id or Password is invalid !";
+	}
+}
+?>
